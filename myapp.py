c292414b1ca66f6894966b0b8034c318050ff805
STCcode <1101satyamtiwari@gmail.com>
2025-08-06 03:15:10 -0700

    myapp

--- myapp.py
+++ myapp.py
@@ -1,11 +1,16 @@
 
 import route
+import os
 # from sparkEntry import spark
 # spark = spark
 
 
 
-if __name__ == '__main__':	
-	route.app.debug = True
-	route.app.run(host='0.0.0.0',port=5001)
-	route.app.run(debug=True)
+# if __name__ == '__main__':	
+# 	route.app.debug = True
+# 	route.app.run(host='0.0.0.0',port=5001)
+# 	route.app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render provides the PORT variable
+    route.app.run(host='0.0.0.0', port=port, debug=True)

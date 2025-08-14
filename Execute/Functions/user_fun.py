a94fad020ac0ea242d94c81fbf3292c4e8d90c34
STCcode <1101satyamtiwari@gmail.com>
2025-08-14 17:04:04 +0530

    upenty

--- Execute/Functions/user_fun.py
+++ Execute/Functions/user_fun.py
@@ -306,7 +306,7 @@ def update_entity_table():
             formData = request.get_json()
 
            
-            formlist = (formData.get('scripname'),formData.get('scripcode'),formData.get('benchmark'),formData.get('category'),formData.get('subcategory'),formData.get('nickname'),datetime.now(),formData.get('id'),formData.get('isin')
+            formlist = (formData.get('scripname'),formData.get('scripcode'),formData.get('benchmark'),formData.get('category'),formData.get('subcategory'),formData.get('nickname'),formData.get('isin'),datetime.now(),formData.get('id')
             )
 
             updated_rows = queries.update_entity_table(formlist)

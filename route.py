5367ff50cd29aadb4c9ce4c0a4d088425bf805f7
STCcode <1101satyamtiwari@gmail.com>
2025-08-22 15:30:52 +0530

    AifRoute

--- route.py
+++ route.py
@@ -135,7 +135,7 @@ app.add_url_rule('/getCamByid',view_func=user_fun.getCamByid,methods=['GET','POS
 
 app.add_url_rule('/InsertAifData' ,view_func=user_fun.InsertAifData,methods=['GET','POST'])
 app.add_url_rule('/getAllAif',view_func=user_fun.getAllAif,methods=['GET','POST'])
-app.add_url_rule('/getAifActionTablebyId ',view_func=user_fun.getAifActionTablebyId,methods=['GET','POST'])
+app.add_url_rule('/getAifActionTablebyId',view_func=user_fun.getAifActionTablebyId,methods=['GET','POST'])
 
 app.add_url_rule('/getAifEntity',view_func=user_fun.getAifEntity,methods=['GET','POST'])
 

857ed007176e568fc8111532052eb8d7f488fc29
STCcode <1101satyamtiwari@gmail.com>
2025-09-12 12:47:40 +0530

    error resolve

--- Execute/queries.py
+++ Execute/queries.py
@@ -825,7 +825,7 @@ def getPmsClientEntity():
           print("Error in getingroleRecord query==========================",e)
           return middleware.exe_msgs(responses.queryError_501,str(e.args),'1023310')   
      
-def getPmsEquityDetailbyId():
+def getPmsEquityDetailbyId(entity_id):
      try:
         sql=" SELECT * FROM tbl_entity WHERE entityid = %s;"
         data=''

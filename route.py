bb20678849a3e5579d1a262d4323a5a9e00ebd1f
STCcode <1101satyamtiwari@gmail.com>
2025-09-12 16:37:02 +0530

    getallMfEquityUnderlyingCount

--- route.py
+++ route.py
@@ -234,7 +234,7 @@ app.add_url_rule('/getEquityActionTable',view_func=user_fun.getEquityActionTable
 
 
 # ======================================== Get allMfEquityUnderlyingCount Start============================
-app.add_url_rule('/GetallMfEquityUnderlyingCount',view_func=user_fun.GetallMfEquityUnderlyingCount,methods=['GET','POST'])
+app.add_url_rule('/getallMfEquityUnderlyingCount',view_func=user_fun.GetallMfEquityUnderlyingCount,methods=['GET','POST'])
 # ======================================== Get allMfEquityUnderlyingCount End============================
 
 # ======================================Get All Home Equity======================================

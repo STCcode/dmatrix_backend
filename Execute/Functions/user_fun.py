692a3a16f6f81147783dc7521b238f434b413b16
STCcode <1101satyamtiwari@gmail.com>
2025-09-19 12:30:35 +0530

    getAllActionTableOfETFEquity

--- Execute/Functions/user_fun.py
+++ Execute/Functions/user_fun.py
@@ -1540,47 +1540,22 @@ def getAllETFEquity():
             print("Error in getting role data=============================", e)
             return  make_response(middleware.exe_msgs(responses.getAll_501,str(e.args),'1023500'),500)  
         
-def  getAllActionTableOfETFEquity ():
-    try:
-        entity_id = None
-
-        # Handle GET → from query params
-        if request.method == 'GET':
-            entity_id = request.args.get('entityid')
-
-        # Handle POST → from JSON or form-data
-        elif request.method == 'POST':
-            if request.is_json:
-                entity_id = request.json.get('entityid')
+def getAllActionTableOfETFEquity():
+     if request.method == 'GET':
+        try:
+            data=queries.getAllActionTableOfETFEquity()
+            if type(data).__name__  != "list":
+                if data.json:
+                    result=data
+                    status=500
             else:
-                entity_id = request.form.get('entityid')
-
-        if not entity_id:
-            return make_response(
-                middleware.exe_msgs(responses.getAll_501, "Missing entityid parameter", '1023501'),
-                400
-            )
-
-        data = queries.getAllActionTableOfETFEquity (entity_id)
-
-        # Return proper response
-        if isinstance(data, list):
-            result = middleware.exs_msgs(data, responses.getAll_200, '1023200')
-            status = 200
-        else:
-            result = data
-            status = 500
-
-        return make_response(result, status)
-
-    except Exception as e:
-        print("Error in getting underlying by id:", e)
-        return make_response(
-            middleware.exe_msgs(responses.getAll_501, str(e.args), '1023500'),
-            500
-        )
-
-
+                result=middleware.exs_msgs(data,responses.getAll_200,'1023200')
+                status=200
+                        
+            return make_response(result,status)
+        except Exception as e:
+            print("Error in getting role data=============================", e)
+            return  make_response(middleware.exe_msgs(responses.getAll_501,str(e.args),'1023500'),500)  
 
 
 def  getETFDetailsEquityById ():

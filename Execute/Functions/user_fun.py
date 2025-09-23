653e52b7c5bc6527b09858d23c44b52e2bcc26b1
STCcode <1101satyamtiwari@gmail.com>
2025-09-23 17:05:29 +0530

    newAPIgetallMfSectorUnderlyingCount

--- Execute/Functions/user_fun.py
+++ Execute/Functions/user_fun.py
@@ -2799,6 +2799,26 @@ def GetallMfEquityUnderlyingCount():
         except Exception as e:
             print("Error in getting role data=============================", e)
             return  make_response(middleware.exe_msgs(responses.getAll_501,str(e.args),'1023500'),500) 
+        
+
+def GetallMfSectorUnderlyingCount():
+     if request.method == 'GET':
+        try:
+            data=queries.GetallMfSectorUnderlyingCount()
+
+
+            if not isinstance(data, list):
+                 result = data
+                 status = 500
+            else:
+                data = serialize_dates(data)
+                result = middleware.exs_msgs(data, responses.getAll_200, '1023200')
+                status = 200
+                        
+            return make_response(result,status)
+        except Exception as e:
+            print("Error in getting role data=============================", e)
+            return  make_response(middleware.exe_msgs(responses.getAll_501,str(e.args),'1023500'),500)         
 
 
 # ======================================== Get allMfEquityUnderlyingCount End============================

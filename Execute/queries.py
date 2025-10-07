87a730becb096046c329f48255060f2f80d73e15
STCcode <1101satyamtiwari@gmail.com>
2025-10-07 17:53:01 +0530

    updateMF

--- Execute/queries.py
+++ Execute/queries.py
@@ -261,7 +261,7 @@ def updateMFDetailActionTableRow(data, record_id):
 
     except Exception as e:
         print("Error in updateMFDetailActionTableRow query ==========================", e)
-        return exe_msgs(responses.update_501, str(e.args), '1020501')
+        return (responses.update_501, str(e.args), '1020501')
 
 
 def deleteMFDetailActionTableRow(entity_id):
